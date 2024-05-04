d09569c49ec6daf4dab873bc14071ca6d8267c3e
janithrs <janithravindu@gmail.com>
2024-05-04 20:13:52 +0200

    remove sparkles

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -19,26 +19,6 @@ export const HeroSection: FC = () => (
           className="mx-auto max-w-[600px] text-white px-8"
           words={`Adler is an ensemble of singers and musicians hailing from St.Anthony's College, Kandy.`}
         />
-        <div className="w-full md:w-[40rem] h-40 relative flex justify-center">
-          {/* Gradients */}
-          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-          {/* Core component */}
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={0.5}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-
-          {/* Radial Gradient to prevent sharp edges */}
-          <div className="absolute inset-0 w-full h-full bg-[#4C1C24] [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-        </div>
       </div>
     </div>
   </section>
