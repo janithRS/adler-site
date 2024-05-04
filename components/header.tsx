0923d8c5256be5ce311c7f284f5e331af7e2eb48
janithrs <janithravindu@gmail.com>
2024-05-04 19:54:18 +0200

    update hero

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { FC } from "react";
 
 export const Header: FC = () => (
-  <nav className="sticky top-0 bg-red-950 z-50">
+  <nav className="sticky top-0 bg-[#4C1C24] z-50">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,9 +2,10 @@
 import Image from "next/image";
 import { FC } from "react";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { SparklesCore } from "./ui/sparkles";
 
 export const HeroSection: FC = () => (
-  <section className="w-full bg-red-950 text-white py-12 lg:py-28 md:pt-4">
+  <section className="w-full bg-[#4C1C24] text-white py-12 lg:py-28 md:pt-4">
     <div className="container flex flex-col items-center justify-center px-4 space-y-4 md:px-6">
       <div className="flex flex-col items-center justify-center text-center space-y-2">
         <Image
@@ -18,6 +19,26 @@ export const HeroSection: FC = () => (
           className="mx-auto max-w-[600px] text-white px-8"
           words={`Adler is an ensemble of singers and musicians hailing from St.Anthony's College, Kandy.`}
         />
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+          {/* Core component */}
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-[#4C1C24] [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
       </div>
     </div>
   </section>
